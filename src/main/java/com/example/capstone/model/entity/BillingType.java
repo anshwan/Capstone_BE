ef051463020b5ec2e7649a8b3cfd93eb7c643d05
안승환 <ansh10041@gmail.com>
2025-10-01 17:26:45 +0900

    수정

--- src/main/java/com/example/capstone/model/entity/BillingType.java
+++ src/main/java/com/example/capstone/model/entity/BillingType.java
@@ -1,5 +1,7 @@
 package com.example.capstone.model.entity;
 
 public enum BillingType {
-    FREE, MONTHLY_SUBSCRIPTION, ONE_TIME_PURCHASE
+    free,
+    monthly_subscription,
+    one_time_purchase
 }
